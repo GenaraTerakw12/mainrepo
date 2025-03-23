4e9a4f1f4e2b62e87df8b995cbdf8e3eab840466
hanyenfodev <hany@enfodev.com>
2025-03-23 07:20:20 +0200

    sdf

--- setup.bat
+++ setup.bat
@@ -1,13 +1,14 @@
 cd blue 
+echo off
 
-# To install BlueStacks 5 Nougat 64-bit:
-#blue.exe --defaultImageName Nougat64 --imageToLaunch Nougat64
+## To install BlueStacks 5 Nougat 64-bit:
+##blue.exe --defaultImageName Nougat64 --imageToLaunch Nougat64
 
-# To install BlueStacks 5 Pie 64-bit:
-#blue.exe --defaultImageName Pie64 --imageToLaunch Pie64
+## To install BlueStacks 5 Pie 64-bit:
+##blue.exe --defaultImageName Pie64 --imageToLaunch Pie64
 
-# To install BlueStacks 5 Android 11:
-#blue.exe --defaultImageName Rvc64 --imageToLaunch Rvc64
+## To install BlueStacks 5 Android 11:
+##blue.exe --defaultImageName Rvc64 --imageToLaunch Rvc64
 
-# To install BlueStacks 5 Android 13:
+## To install BlueStacks 5 Android 13:
 blue.exe --defaultImageName Tiramisu64 --imageToLaunch Tiramisu64
