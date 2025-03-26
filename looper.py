73a1739c656f55f334078b3f57ef3ccba4a795ba
hanyenfodev <hany@enfodev.com>
2025-03-26 08:17:13 +0200

    upload

--- looper.py
+++ looper.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime 
 import time 
+import sys
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler(sys.stdout)])
 
